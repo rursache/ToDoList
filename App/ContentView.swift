3542ed83c6219ccd97e9e2f81369b37775d932e2
Radu Ursache <radu@ursache.ro>
2024-07-01 16:35:22 +0300

    - initial project creation - added app icon - added app main colors - initial setup

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -24,15 +24,11 @@ struct ContentView: View {
                 }
                 .onDelete(perform: deleteItems)
             }
-#if os(macOS)
             .navigationSplitViewColumnWidth(min: 180, ideal: 200)
-#endif
             .toolbar {
-#if os(iOS)
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
-#endif
                 ToolbarItem {
                     Button(action: addItem) {
                         Label("Add Item", systemImage: "plus")
